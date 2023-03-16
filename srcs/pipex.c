2373dce56bdb0ae1d41af79a441ada306ee4130f
Marco Chatillon <marcochatillon13@gmail.com>
2023-03-16 17:44:48 +0000

    added envp

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,16 +6,16 @@
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:02:12 by mdoroana          #+#    #+#             */
-/*   Updated: 2023/03/08 15:25:06 by mdoroana         ###   ########.fr       */
+/*   Updated: 2023/03/16 15:51:14 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **envp)
 {
-	if (ac < 5)
+	if (ac != 5)
 		exit_prog(1, "Wrong number of arguments");
-	parse_args
+	parse;
 	return (0);
 }
