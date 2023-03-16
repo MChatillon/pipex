b64793ccbec181a518551151058251e196c75197
Marco Chatillon <marcochatillon13@gmail.com>
2023-03-16 17:42:46 +0000

    Added pipex struct and functions

--- headers/pipex.h
+++ headers/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:50:16 by mdoroana          #+#    #+#             */
-/*   Updated: 2023/03/08 16:53:45 by mdoroana         ###   ########.fr       */
+/*   Updated: 2023/03/16 17:15:44 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,17 @@
 
 # include "ft_printf.h"
 
-typedef struct 
-{
-	/* data */
-};
+typedef struct s_pipex{
+	char	**cmd;
+	char	*path;
+	int		infile;
+	int		outfile;
+}t_pipex;
 
 void	exit_prog(int status, char *s);
+char	**ft_split(char **split, char *str, int count);
+void	parse(char **av, char **envp);
+char	*get_path(char **envp);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 
 #endif
