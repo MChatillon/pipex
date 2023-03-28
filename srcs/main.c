ed2669ff5790a80c26d0e1eaefd52e78f2aa9bd9
Marco Chatillon <marcochatillon13@gmail.com>
2023-03-28 18:04:18 +0100

    created pipe_fd and completed the parse

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:02:12 by mdoroana          #+#    #+#             */
-/*   Updated: 2023/03/28 16:07:32 by mdoroana         ###   ########.fr       */
+/*   Updated: 2023/03/28 17:57:59 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 int	main(int ac, char **av, char **envp)
 {
+	int	pipe_fd[2];
+
 	if (ac != 5)
 		exit_prog(1, "Wrong number of arguments");
 	parse(av, envp);
+	if (pipe(pipe_fd) == -1)
+		exit_prog(1, "Pipe error");
 	return (0);
 }
