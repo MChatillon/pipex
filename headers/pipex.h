ceca6a8b88a6077a2382703ac169f68622172d08
Marco Chatillon <marcochatillon13@gmail.com>
2023-03-28 17:59:16 +0100

    added list struct and funtions to the header

--- headers/pipex.h
+++ headers/pipex.h
@@ -6,7 +6,7 @@
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 17:50:16 by mdoroana          #+#    #+#             */
-/*   Updated: 2023/03/16 17:15:44 by mdoroana         ###   ########.fr       */
+/*   Updated: 2023/03/28 16:09:05 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define PIPEX_H
 
 # include "ft_printf.h"
+# include <stdlib.h>
+# include <unistd.h>
 
 typedef struct s_pipex{
 	char	**cmd;
@@ -22,10 +24,24 @@ typedef struct s_pipex{
 	int		outfile;
 }t_pipex;
 
+typedef struct s_list
+{
+	char			**cmd;
+	char			*path;
+	struct s_list	*next;
+}t_list;
+
+// Pipex functions
+
 void	exit_prog(int status, char *s);
 char	**ft_split(char **split, char *str, int count);
 void	parse(char **av, char **envp);
 char	*get_path(char **envp);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+t_pipex	*px(void);
+
+// List funtions
+void	*list_add_last(t_list **list, t_list *new);
+t_list	*new_list(char **cmd);
 
 #endif
