b5f221831ddd8e79b8916ccb91767e3bfcb07e7e
Marco Chatillon <marcochatillon13@gmail.com>
2023-03-28 18:00:53 +0100

    created a free_list and a new_list function

--- srcs/list_utils.c
+++ srcs/list_utils.c
@@ -6,11 +6,24 @@
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 17:35:29 by mdoroana          #+#    #+#             */
-/*   Updated: 2023/03/24 17:41:27 by mdoroana         ###   ########.fr       */
+/*   Updated: 2023/03/28 17:44:59 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+t_list	*new_list(char **cmd)
+{
+	t_list	*new;
+
+	new = (t_list *)malloc(sizeof(t_list));
+	if (!new)
+		return (NULL);
+	new->path = NULL;
+	new->cmd = cmd;
+	new->next = NULL;
+	return (new);
+}
 
 void	*list_add_last(t_list **list, t_list *new)
 {
@@ -25,4 +38,16 @@ void	*list_add_last(t_list **list, t_list *new)
 	}
 	last = ft_lstlast(*lst);
 	last->next = new;
+}
+
+void	free_list(t_list **cmd)
+{
+	t_list	*tmp;
+
+	while (*cmd)
+	{
+		tmp = *cmd;
+		*cmd = (*cmd)->next;
+		free(tmp);
+	}
 }
